54b5cdc2cdb2f93c9985479c32aa626d106aa1b0
jinyang <jinyangcruise@163.com>
2023-10-05 23:38:31 +0800

    数据库workbench继续完善：修复truncate table日志问题

--- addons/gdsql/tree_databases.gd
+++ addons/gdsql/tree_databases.gd
@@ -526,10 +526,10 @@ func truncate_table_from_config(db_name: String, table_name: String) -> void:
 	else:
 		msgs.push_back("1 file: %s intended to move to trash but not found." % data_path)
 		
-	#
+	# create empty file
 	__CONF_MANAGER.get_conf(data_path, "").clear()
 	__CONF_MANAGER.save_conf_by_origin_password(data_path)
-	msgs.push_back("1 file: %s has been overwritten to an empty file.")
+	msgs.push_back("1 file: %s has been overwritten to an empty file." % data_path)
 	
 	mgr.add_log_history.emit("OK", begin_time, action, msgs)
 	
