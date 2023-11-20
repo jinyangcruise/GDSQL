0722be632c910fb6f34c81e3b30bdfa4efe844f4
jinyang <jinyangcruise@163.com>
2023-11-20 22:13:33 +0800

    数据库workbench继续完善：mgr创建自定义对话框增加返回值，返回该对话框；evaluate_command可以动态创建脚本解析表达式

--- addons/gdsql/singletons/gdsql_workbench_manager.gd
+++ addons/gdsql/singletons/gdsql_workbench_manager.gd
@@ -247,7 +247,7 @@ func create_custom_dialog(datas: Array[Array],
 confirmed_callback_before_close: Callable = Callable(), 
 canceled_callback_before_close: Callable = Callable(),
 defered_callback: Callable = Callable(),
-min_size: Vector2i = Vector2i.ZERO):
+min_size: Vector2i = Vector2i.ZERO) -> ConfirmationDialog:
 	var dialog := ConfirmationDialog.new()
 	dialog.dialog_hide_on_ok = false
 	__custom_dialog_datas[dialog] = datas
@@ -412,5 +412,14 @@ func evaluate_command(target: Object, command: String, variable_names = [], vari
 	if error != OK:
 		push_error(expression.get_error_text())
 		return null
-		
-	return expression.execute(variable_values, target, false)
+	var ret = expression.execute(variable_values, target, false)
+	
+	# 对于一些很简单的但Expression又不支持的写法，动态创建脚本
+	if ret == null and target == null and variable_names.is_empty() and variable_values.is_empty():
+		var script = GDScript.new()
+		script.source_code = "extends Object\nvar value = %s" % command
+		script.reload()
+		var obj = script.new()
+		ret = obj.value
+		obj.free()
+	return ret
