ce1163bc134ab9d8277e5b5d3b915b5dab38219a
jinyang <jinyangcruise@163.com>
2024-06-10 19:07:08 +0800

    XML Editor search help

--- addons/gdsql/gxml/editor/search_herlp.gd
+++ addons/gdsql/gxml/editor/search_herlp.gd
@@ -5,6 +5,7 @@ extends ConfirmationDialog
 @onready var case_sensitive_button: Button = $vbox/hbox/case_sensitive_button
 @onready var filter_combo: OptionButton = $vbox/hbox/filter_combo
 @onready var results_tree: Tree = $vbox/results_tree
+@onready var hide_deprecated: CheckButton = $vbox/hbox/hide_deprecated
 
 
 var EDSCALE: float
@@ -41,23 +42,23 @@ func _ready() -> void:
 		item.set_icon(2, get_theme_icon("Object", "EditorIcons"))
 		item.set_text(2, tr("Class"))
 		if GBatisMapperRule.rule[item_name].deprecated:
+			item.set_meta("deprecated", true)
 			item.set_icon(1, get_theme_icon("StatusError", "EditorIcons"))
 			item.set_tooltip_text(1, tr("This class is marked as deprecated."))
 			item.collapsed = true
-		else:
-			item.set_icon(1, get_theme_icon("StatusSuccess", "EditorIcons"))
 		var props_info = GBatisMapperRule.rule[item_name]["attr_list"]
 		for prop in props_info:
 			var p_item = results_tree.create_item(item)
 			p_item.set_icon(0, get_theme_icon("MemberProperty", "EditorIcons"))
 			p_item.set_text(0, prop)
 			if not props_info[prop].support:
+				p_item.set_meta("deprecated", true)
 				p_item.set_icon(1, get_theme_icon("StatusError", "EditorIcons"))
 				p_item.set_tooltip_text(1, tr("This member is marked as deprecated."))
 			else:
 				p_item.set_icon(1, get_theme_icon("StatusSuccess", "EditorIcons"))
 				p_item.set_tooltip_text(1, props_info[prop].desc)
-			p_item.set_text(1, props_info[prop].desc)
+			p_item.set_text(1, props_info[prop].desc.replace("\n", ""))
 			if props_info[prop].required:
 				p_item.set_icon(2, get_theme_icon("CryptoKey", "EditorIcons"))
 			else:
@@ -71,6 +72,8 @@ func _ready() -> void:
 			e_item.set_text(0, element)
 			e_item.set_icon(2, get_theme_icon("Object", "EditorIcons"))
 			e_item.set_text(2, tr("Children"))
+			
+	_on_hide_deprecated_toggled(hide_deprecated.button_pressed)
 		
 		
 func EDITOR_GET(n: String):
@@ -122,20 +125,70 @@ func fract(value):
 	return value - floor(value)
 
 func _search_box_gui_input(event: InputEvent) -> void:
-	pass # Replace with function body.
-
-
+	if not event is InputEventKey:
+		return
+	var key = event as InputEventKey
+	match key.keycode:
+		KEY_UP, KEY_DOWN, KEY_PAGEUP, KEY_PAGEDOWN:
+			results_tree.grab_focus()
+			push_input(key)
+			search_box.accept_event()
+			search_box.grab_focus()
+			
+			
 func _search_box_text_changed(new_text: String) -> void:
-	pass # Replace with function body.
+	_update_results()
 
 
 func _update_results() -> void:
-	pass # Replace with function body.
-
-
+	var hide_deprecated = hide_deprecated.button_pressed
+	var search_str = search_box.text.to_lower()
+	
+	for item in results_tree.get_root().get_children():
+		if search_str != "":
+			if not item.get_text(0).to_lower().contains(search_str):
+				item.visible = false
+			else:
+				item.visible = true
+		else:
+			item.visible = true
+			
+		if not item.visible:
+			for c_item in item.get_children():
+				if c_item.get_text(0).to_lower().contains(search_str):
+					item.visible = true
+					c_item.visible = true
+				else:
+					c_item.visible = false
+		else:
+			for c_item in item.get_children():
+				c_item.visible = true
+				
+		if not item.visible:
+			continue
+			
+		if item.get_meta("deprecated", false):
+			item.visible = not hide_deprecated
+			continue
+		for p_item in item.get_children():
+			if p_item.get_meta("deprecated", false):
+				p_item.visible = not hide_deprecated
+				
+	if results_tree.get_selected():
+		results_tree.scroll_to_item(results_tree.get_selected(), true)
+		
 func _filter_combo_item_selected(index: int) -> void:
 	pass # Replace with function body.
 
 
 func _confirmed() -> void:
 	pass # Replace with function body.
+
+
+func _on_hide_deprecated_toggled(toggled_on: bool) -> void:
+	_update_results()
+
+
+func _on_visibility_changed() -> void:
+	if visible:
+		search_box.grab_focus()
