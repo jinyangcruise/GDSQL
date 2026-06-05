c806b10a4c64ee3f69850a90a7603c0d3d0180fc
jinyangcruise <jinyangcruise@gmail.com>
2026-06-05 17:36:41 +0800

    Continue sql text file

--- addons/gdsql/tabs/sql_file/code_edit.gd
+++ addons/gdsql/tabs/sql_file/code_edit.gd
@@ -1,6 +1,11 @@
 @tool
 extends CodeEdit
 
+@export var button_run_edit: Button
+
+var in_run_edit = false
+var in_run_edit_shortcut_feedback = false
+
 func _can_drop_data(_position, data):
 	# { "type": "files", "files": ["res://src/dao/t_hero.gdmappergraph"], "from": @Tree@6840:<Tree#603409380691> }
 	if data is Dictionary:
@@ -20,4 +25,31 @@ func _drop_data(_position, data):
 				GDSQL.WorkbenchManager.open_sql_graph_file_tab.emit(i)
 			elif i.ends_with(".gdmappergraph"):
 				GDSQL.WorkbenchManager.open_mapper_graph_file_tab.emit(i)
-	
+				
+func _gui_input(event: InputEvent) -> void:
+	if in_run_edit_shortcut_feedback:
+		if event is InputEventKey:
+			accept_event()
+		return
+	if button_run_edit.shortcut.matches_event(event):
+		in_run_edit = true
+		if event.is_released():
+			_button_run_edit_pressed()
+		accept_event()
+		return
+	elif in_run_edit:
+		in_run_edit = false
+		_button_run_edit_pressed()
+		accept_event()
+		return
+		
+func _button_run_edit_pressed():
+	button_run_edit.pressed.emit()
+	var normal_sb = button_run_edit.get_theme_stylebox("normal")
+	var hover_pressed_sb = button_run_edit.get_theme_stylebox("hover_pressed")
+	button_run_edit.add_theme_stylebox_override("normal", hover_pressed_sb)
+	in_run_edit_shortcut_feedback = true
+	await get_tree().create_timer(ProjectSettings.get_setting("gui/timers/button_shortcut_feedback_highlight_time", 0.2)).timeout
+	in_run_edit_shortcut_feedback = false
+	if button_run_edit:
+		button_run_edit.add_theme_stylebox_override("normal", normal_sb)
--- addons/gdsql/tabs/sql_file/sql_file.gd
+++ addons/gdsql/tabs/sql_file/sql_file.gd
@@ -8,6 +8,7 @@ signal change_tab_title(page: Control, title: String)
 @onready var button_commit: Button = %ButtonCommit
 @onready var button_rollback: Button = %ButtonRollback
 @onready var button_auto_commit: Button = %ButtonAutoCommit
+@onready var results_tab: TabContainer = %ResultsTab
 
 
 var code_edit: CodeEdit:
@@ -85,3 +86,12 @@ func load_sql_file(path: String):
 	set_meta("is_file", true)
 	set_meta("file_path", path)
 	set_meta("file_name", path.get_file())
+	
+func _on_button_run_edit_pressed() -> void:
+	# TODO 已知sql语句是用分号来进行划分的，在一行中，如果出现 -- （两个减号 + 空格）或 # 或 /*  */，
+	# 则表示注释内容。
+	# 我们的目标是：找到光标所在行的单个sql语句。这个sql语句可能跨行了，在光标所在行的上方或下方，都有可能。
+	# 另外，光标所在行还有可能存在多个sql语句，那么就要考虑光标的column是处于哪个sql语句了。此外，还要
+	# 把注释部分去掉。
+	var sql = code_edit.get_line(code_edit.get_caret_line(0))
+	printt("sql:", sql)
